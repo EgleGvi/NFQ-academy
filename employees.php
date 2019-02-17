ceebf4c1fbbb38ea8039dfa1ea0ad3bc5c03d0f6
EgleGvi <e.gvildyte@gmail.com>
2019-02-17 19:25:00 +0200

    updated clients table

--- employees.php
+++ employees.php
@@ -31,26 +31,26 @@
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <div class="table">
-                        <ul id="menu" class="client-info-wrapper">
-                            <li>Data ir laikas</li>
-                            <li>Vardas Pavardė</li>
-                            <li>Pastabos</li>
-                            <li>Veiksmai</li>
+                    <div>
+                        <ul id="menu">
+                            <li class="list-menu">Data ir laikas</li>
+                            <li class="list-menu">Vardas Pavardė</li>
+                            <li class="list-menu">Pastabos</li>
+                            <li class="list-menu">Veiksmai</li>
                         </ul>
-                        <ul id="menu" class="client-info-wrapper2">
-                            <li class="client1">2019-01-23 12:45</li>
-                            <li class="client1">Ona Onutė</li>
-                            <li class="client1">Plaukų dažymas</li>
-                            <li class="client1"><a class="del-btn" href="#">Atšaukti</a></li>
+                        <ul id="menu">
+                            <li>2019-01-23 12:45</li>
+                            <li>Ona Onutė</li>
+                            <li></li>
+                            <li><a class="del-btn" href="#">Atšaukti</a></li>
                        </ul>
-                        <ul id="menu" class="client-info-wrapper3">
+                        <ul id="menu">
                             <li>2019-02-23 16:00</li>
                             <li>Onytė Ona</li>
                             <li>Plaukų kirpimas</li>
                             <li><a class="del-btn" href="#">Atšaukti</a></li>
                         </ul>
-                        <ul id="menu" class="client-info-wrapper4">
+                        <ul id="menu">
                             <li>2019-02-23 12:30</li>
                             <li>Ona Onutė</li>
                             <li>Šukuosena</li>
